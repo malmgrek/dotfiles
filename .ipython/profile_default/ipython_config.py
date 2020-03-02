c844025390963785ade90e0abbfc03b72631200a
Stratos Staboulis <stratos.staboulis@leanheat.com>
2020-03-02 22:51:36 +0200

    Add IPython powerline

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,8 @@
+"""This works with the develop branch of powerline
+
+See https://github.com/powerline/powerline/issues/1953
+"""
+from powerline.bindings.ipython.since_7 import PowerlinePrompts
+c = get_config()
+c.TerminalInteractiveShell.simple_prompt = False
+c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
