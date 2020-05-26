b4248e635b73699257e5dac40f6bcbcfe276448a
Stratos Staboulis <stratos.staboulis@leanheat.com>
2020-05-26 22:23:45 +0300

    Update Doom Emacs as default profile

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,2 +1,2 @@
-(("default" . ((user-emacs-directory . "~/.emacs.d")))
- ("doom" . ((user-emacs-directory . "~/doom-emacs"))))
+(("custom" . ((user-emacs-directory . "~/.emacs.d")))
+ ("default" . ((user-emacs-directory . "~/doom-emacs"))))
