525516bf718a80155d9c5d97e57d64f068f63a3d
Stratos Staboulis <stratos.staboulis@leanheat.com>
2020-06-05 21:09:56 +0300

    Update conda plugin with unicode

--- .oh-my-zsh/custom/plugins/conda/conda.plugin.zsh
+++ .oh-my-zsh/custom/plugins/conda/conda.plugin.zsh
@@ -1,4 +1,4 @@
 function conda_prompt_info(){
   [[ -n ${CONDA_DEFAULT_ENV} ]] || return
-  echo "${CONDA_DEFAULT_ENV:t}"
+  echo "${ZSH_THEME_CONDA_PROMPT_PREFIX:=[}${CONDA_DEFAULT_ENV:t}${ZSH_THEME_CONDA_PROMPT_SUFFIX:=]}"
 }
